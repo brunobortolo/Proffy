5ff0cbb58fcf48f317dff436199bbdd40a5815a4
Bruno Bortolo <brunobortolinho@gmail.com>
2020-08-13 15:35:21 -0300

    Proffy routes link back-end done

--- Proffy/web/src/components/TeacherItem/index.tsx
+++ Proffy/web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,54 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    name: string;
+    avatar: string;
+    whatsapp: number;
+    bio: string;
+    subject: string;
+    cost: number;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/35694424?s=460&u=484e1ebc087a8de097fdc67da1fcb727b9b10d7e&v=4" alt="Bruno Bortolo"/>
+                <img src={ teacher.avatar } alt={ teacher.name } />
                 <div>
-                    <strong>Bruno Bortolo</strong>
-                    <span>Química</span>
+                    <strong>{ teacher.name }</strong>
+                    <span>{ teacher.subject }</span>
                  </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada
-                <br /><br />
-                Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma de minha explosões. 
-            </p>
+            <p>{ teacher.bio }</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ { teacher.cost }</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                 </button>
+                 </a>
             </footer>
         </article>
     )
